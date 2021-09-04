e2e280d8c5c873c883388e2cca4dbaf74f6f9481
Yanan Lyu <yanan.lyu@gmail.com>
2021-09-04 01:41:04 -0700

    add authMiddleware service

--- pkg/app/authMiddleware.go
+++ pkg/app/authMiddleware.go
@@ -1,7 +1,11 @@
 package app
 
 import (
+	"fmt"
+	"github.com/golang-jwt/jwt"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/go-chi/chi"
 )
@@ -9,12 +13,53 @@ import (
 func authMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		if r.URL.Path == "/api/v1/login" || r.URL.Path == "/api/v1/health" || !router.Match(chi.NewRouteContext(), r.Method, r.URL.Path) {
+		if r.URL.Path == "/api/v1/auth/login" || r.URL.Path == "/api/v1/health" || !router.Match(chi.NewRouteContext(), r.Method, r.URL.Path) {
 			next.ServeHTTP(w, r)
 			return
 		}
 
-		http.Error(w, "auth middleware error", http.StatusBadRequest)
-		// next.ServeHTTP(w, r)
+		err := TokenValid(r)
+		if err != nil {
+			http.Error(w, "auth middleware error", http.StatusBadRequest)
+			return
+		}
+
+		next.ServeHTTP(w, r)
 	})
+}
+
+func TokenValid(r *http.Request) error {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return err
+	}
+	return nil
+}
+
+func VerifyToken(r *http.Request) (*jwt.Token, error) {
+	tokenString := ExtractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func ExtractToken(r *http.Request) string {
+	bearToken := r.Header.Get("Authorization")
+	//normally Authorization the_token_xxx
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
 }
